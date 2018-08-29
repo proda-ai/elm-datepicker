5c376b50814865eadccb172d3419c8a7fc72de03
Jakob Demler <jdemler@curry-software.com>
2018-08-29 19:35:28 +0200

    updated tests

--- nightwatch-tests/simple.js
+++ nightwatch-tests/simple.js
@@ -21,7 +21,7 @@ module.exports = {
     client.url(url);
     client.expect.element(textInputSelector).to.be.present.before(defaultWait);
     client.click(textInputSelector);
-    slowlySendKeys(client, textInputSelector, "1 Jan 1980");
+    slowlySendKeys(client, textInputSelector, "1980-01-01");
     client.expect.element(topLeftDaySelector).to.be.present.before(defaultWait);
     client.click(topLeftDaySelector);
     client.expect.element(textInputSelector).value.to.equal("1969/06/29").before(defaultWait);
@@ -32,12 +32,12 @@ module.exports = {
     client.url(url);
     client.expect.element(textInputSelector).to.be.present.before(defaultWait);
     client.click(textInputSelector);
-    slowlySendKeys(client, textInputSelector, "1 Jan 1980");
+    slowlySendKeys(client, textInputSelector, "1980-01-01");
     client.sendKeys(textInputSelector, client.Keys.ENTER);
     client.expect.element(topLeftDaySelector).to.be.present.before(defaultWait);
     client.expect.element(".elm-datepicker--row:first-child .elm-datepicker--day:nth-child(3)")
       .to.have.attribute('class').which.contains('elm-datepicker--picked');
-    client.expect.element("h1").text.to.equal("Jan 1, 1980");
+    client.expect.element("h1").text.to.equal("Jan 01, 1980");
 
     // now we click on another value, to make sure the input is updated
     client.click(topLeftDaySelector);
@@ -46,20 +46,17 @@ module.exports = {
     client.end();
   },
 
-  // This test has been commented out, we are currently unable to find a
-  // satisfactory solution for https://github.com/elm-community/elm-datepicker/issues/63.
-  //
-  // 'Characters should not be dropped when entering text quickly' : function (client) {
-  //
-  //   const longTextExample = "The quick brown fox jumped over the lazy dog";
-  //
-  //   client.url(url);
-  //   client.expect.element(textInputSelector).to.be.present.before(defaultWait);
-  //   client.click(textInputSelector);
-  //   client.sendKeys(textInputSelector, longTextExample);
-  //   client.expect.element(textInputSelector).value.to.equal(longTextExample).before(defaultWait);
-  //   client.end();
-  // },
+  'Characters should not be dropped when entering text quickly' : function (client) {
+
+    const longTextExample = "The quick brown fox jumped over the lazy dog";
+
+    client.url(url);
+    client.expect.element(textInputSelector).to.be.present.before(defaultWait);
+    client.click(textInputSelector);
+    client.sendKeys(textInputSelector, longTextExample);
+    client.expect.element(textInputSelector).value.to.equal(longTextExample).before(defaultWait);
+    client.end();
+  },
 
   'Manually entered text should not be cleared when the view re-renders' : function (client) {
 
